c3e5538955843703d2afe56df728e46848e917fb
susantokun <susantokun.com@gmail.com>
2022-06-07 16:30:07 +0700

    Update button actions

--- resources/js/backend/components/buttons/ButtonActions.js
+++ resources/js/backend/components/buttons/ButtonActions.js
@@ -3,7 +3,7 @@ import { Edit, Eye, Trash2 } from 'react-feather';
 
 export function ButtonEdit(props) {
     return (
-        <a href={props.path} className="p-1.5 inline-flex items-center justify-center text-white rounded-md shadow-sm bg-warning">
+        <a href={props.path} className="p-1.5 inline-flex items-center justify-center text-white rounded-md shadow-sm bg-warning focus:border-warning/60 focus:ring focus:ring-warning/10 focus:ring-opacity-50 hover:bg-warning/80">
            <Edit className="w-4 h-4" />
         </a>
     );
@@ -11,7 +11,7 @@ export function ButtonEdit(props) {
 
 export function ButtonShow(props) {
     return (
-        <a href={props.path} className="p-1.5 inline-flex items-center justify-center text-white rounded-md shadow-sm bg-info">
+        <a href={props.path} className="p-1.5 inline-flex items-center justify-center text-white rounded-md shadow-sm bg-info focus:border-info/60 focus:ring focus:ring-info/10 focus:ring-opacity-50 hover:bg-info/80">
            <Eye className="w-4 h-4" />
         </a>
     );
@@ -19,7 +19,7 @@ export function ButtonShow(props) {
 
 export function ButtonDelete(props) {
     return (
-        <button className="p-1.5 inline-flex items-center justify-center text-white rounded-md shadow-sm bg-danger">
+        <button className="p-1.5 inline-flex items-center justify-center text-white rounded-md shadow-sm bg-danger focus:border-danger/60 focus:ring focus:ring-danger/10 focus:ring-opacity-50 hover:bg-danger/80">
            <Trash2 className="w-4 h-4" />
         </button>
     );
