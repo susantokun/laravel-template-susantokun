6acb70c0c054c07d1a0d9f489c5739c3298e989d
susantokun <susantokun.com@gmail.com>
2022-06-07 20:49:23 +0700

    Update button secondary and home

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -4,7 +4,7 @@
     <title>Laravel Template Susantokun</title>
     @endsection
 
-    <div class="flex flex-col items-center justify-center min-h-screen px-4 mx-auto leading-8 text-center cursor-pointer select-none max-w-7xl sm:px-6 lg:px-8">
+    <div class="flex flex-col items-center justify-center min-h-screen px-4 mx-auto leading-8 text-center select-none max-w-7xl sm:px-6 lg:px-8">
         <div class="font-mono">Welcome to <span class="underline decoration-sky-500 underline-offset-2">Laravel</span> <span
                 class="underline decoration-pink-500 underline-offset-2"
             >Template</span>
@@ -19,16 +19,18 @@
                     class="text-sm text-gray-200 dark:text-gray-100 px-4 py-1.5 bg-primary rounded-md shadow-md shadow-primary/60"
                 >Dashboard</a>
                 @else
-                <a
-                    href="{{ route('login') }}"
-                    class="text-sm text-gray-200 dark:text-gray-100 px-4 py-1.5 bg-primary rounded-md shadow-md shadow-primary/60"
-                >Log in</a>
+                <a href="{{ route('login') }}">
+                    <x-button-primary>
+                        Log in
+                    </x-button-primary>
+                </a>
 
                 @if (Route::has('register'))
-                <a
-                    href="{{ route('register') }}"
-                    class="text-sm text-gray-800 dark:text-gray-900 px-4 py-1.5 bg-secondary rounded-md shadow-md shadow-secondary/60"
-                >Register</a>
+                <a href="{{ route('register') }}">
+                    <x-button-secondary>
+                        Register
+                    </x-button-secondary>
+                </a>
                 @endif
                 @endauth
             </div>
