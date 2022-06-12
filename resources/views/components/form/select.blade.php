135d8122e14659dbda9517107f0450134981519d
susantokun <susantokun.com@gmail.com>
2022-06-12 18:24:50 +0700

    Fix name

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,6 +1,6 @@
 <div class="col-span-3">
     {{ Form::label(__($label), null, ['class' => 'form-label']) }}
-    {{ Form::select('status', $value, null, array_merge(['class' => 'form-select block w-full mt-1'], $attributes)) }}
+    {{ Form::select($name, $value, null, array_merge(['class' => 'form-select block w-full mt-1'], $attributes)) }}
     @error($name)
     <p class="mt-1 text-xs font-medium text-danger">{{ $message }}</p>
     @enderror
