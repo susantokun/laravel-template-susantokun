7c7bff894955bbc09dc3af5586697883fae33958
susantokun <susantokun.com@gmail.com>
2022-06-25 01:39:18 +0700

    Add modal component

---
+++ resources/js/backend/components/Modal.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
+export default function Modal(props) {
+    return (
+        <Transition appear show={props.isOpen} as={Fragment}>
+            <Dialog
+                as="div"
+                className="fixed inset-0 z-40 overflow-y-auto"
+                onClose={props.closeModal}
+            >
+                <Dialog.Overlay className="fixed inset-0 bg-black opacity-60" />
+                <div className="min-h-screen px-4 text-center">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <Dialog.Overlay className="fixed inset-0" />
+                    </Transition.Child>
+
+                    {/* This element is to trick the browser into centering the modal contents. */}
+                    <span
+                        className="inline-block h-screen align-middle"
+                        aria-hidden="true"
+                    >
+                        &#8203;
+                    </span>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                    >
+                        <div className="inline-flex flex-col items-center justify-center w-full max-w-lg p-6 my-8 overflow-hidden align-middle transition-all transform bg-white shadow-xl select-none rounded-2xl">
+                            {props.children}
+                        </div>
+                    </Transition.Child>
+                </div>
+            </Dialog>
+        </Transition>
+    );
+}
